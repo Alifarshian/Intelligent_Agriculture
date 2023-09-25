d04fa28a71bbc98b3091c7956128a776e194a2ee
Alifarshian <145116382+Alifarshian@users.noreply.github.com>
2023-09-26 01:29:45 +0330

    Update project_course.ino

--- project_course.ino
+++ project_course.ino
@@ -1,29 +1,52 @@
+#include <dht11.h>
+#define DHT11_PIN 7
+dht11 DHT11;
 #include <Servo.h>
 Servo myservo;
 int pos = 0;
-int photocellPin = 0;
-int humidityPin=1;    
+#include <LiquidCrystal.h> 
+int Contrast=80;
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int photocellPin = 0;   
 void setup() {
-  pinMode(6,OUTPUT);
-  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(10,OUTPUT);
   pinMode(A0,INPUT);
-  myservo.attach(9);
+  myservo.attach(13);
   Serial.begin(9600);
+  analogWrite(6,Contrast);
+  lcd.begin(16, 2);
 }
 
 void loop() {
   int lux = analogRead(photocellPin);
+  int chk = DHT11.read(DHT11_PIN);
   Serial.print("brightness=");
   Serial.print(lux);
-  Serial.print(" humidity=");
+  Serial.print(" Temp=");
+  Serial.print((float)DHT11.temperature);
+  Serial.print(" C");
+  Serial.print(" Humidity=");
+  Serial.print((float)DHT11.humidity);
+  Serial.print("%");
+  Serial.print("\n");
   if(lux>=100){
-    digitalWrite(6,HIGH);
-    digitalWrite(7,LOW);
+    digitalWrite(10,HIGH);
+    digitalWrite(8,LOW);
     myservo.write(90);
   }
   if(lux<100){
-    digitalWrite(6,LOW);
-    digitalWrite(7,HIGH);
+    digitalWrite(10,LOW);
+    digitalWrite(8,HIGH);
     myservo.write(0);
   }
+   lcd.setCursor(0,0); 
+   lcd.print("Temp: ");
+   lcd.print(DHT11.temperature);
+   lcd.print((char)223);
+   lcd.setCursor(0, 1);
+   lcd.print("Humidity: ");
+   lcd.print(DHT11.humidity);
+   lcd.print("%");
+   delay(200);
 }
